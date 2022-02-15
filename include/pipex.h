67ec8ddc121fc269ef7e622d0ebebab6b9e24516
Leonardo Moreno <lmoreno@c1r1p8.42quebec.com>
2022-02-15 17:42:34 -0500

    Try Multiprocesos fork()

--- include/pipex.h
+++ include/pipex.h
@@ -10,10 +10,17 @@
 #define READ_END 0 /*index pipe extremo lectura */
 #define WRITE_END 1 /*index pipe extremo escritura */
 
-void start(char **argv, char **env);
+void start(int argc, char **argv, char **env);
 void startchild1(char **argv, int end[2], int fdin, char **env);
-void startchild2(char **argv, int end[2], int *fdout, char **env);
-void startfork(char **argv, int fdin, char **env);
+
+
+
+void startchild2(int argc, char **argv, int end[2], int *fdout, char **env);
+
+void startchild_n(int argc, char **argv, int end[2], char **env, int);
+
+
+void startfork(int argc, char **argv, int fdin, char **env);
 char *parser_path(char *argu, char **env);
 char *parser_cmd(char *argu, char **arg_split);
 //void path_utils(char **arg_split, char **path, char **path2);
--- src/startChilds.c
+++ src/startChilds.c
@@ -6,7 +6,7 @@
 /*   By: lmoreno <leon.moreno@pm.me>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/13 09:58:51 by lmoreno           #+#    #+#             */
-/*   Updated: 2022/02/14 15:48:18 by lmoreno          ###   ########.fr       */
+/*   Updated: 2022/02/15 17:41:18 by lmoreno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,16 +30,43 @@ void	startchild1(char **argv, int end[2], int fdin, char **env)
 	execve(path_cmd, argv_cmd1, NULL);
 }
 
-void	startchild2(char **argv, int end[2], int *fdout, char **env)
+/*void startchild_n(int argc, char **argv, int end[2], char **env, int multi_end[2])
+{
+ // Necesito saber que parameteo estoy enviando
+ // Cual argv estoy, para asi mismo saber cual ejecutar.
+ // Si envio desde la FUNC q llama 3 + i;	
+	ESTA ES LA FUNCION QUE UTILIZO PARA EL BONUS	
+
+ 	char	**cmd;
+	char	*path_cmd;
+	char	*argv_cmd[3];
+
+	cmd = ft_split(argv[argc], ' ');
+	
+	path_cmd = parser_path(cmd[0], env);
+	end[1] = 5;
+	argv_cmd[0] = cmd[0];
+	argv_cmd[1] = cmd[1];
+	argv_cmd[2] = NULL;
+	dup2(end[READ_END], STDIN_FILENO);
+	close(end[READ_END]);
+	dup2(multi_end[WRITE_END], STDOUT_FILENO);
+	close(multi_end[WRITE_END]);
+	execve(path_cmd, argv_cmd, NULL);
+	perror("execve Error");
+	
+}*/ 
+
+void	startchild2(int argc, char **argv, int end[2], int *fdout, char **env)
 {
 	char	**cmd2;
 	char	*argv_cmd2[3];
 	char	*path_cmd;
-
-	*fdout = open(argv[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
+	
+	*fdout = open(argv[argc -1], O_CREAT | O_RDWR | O_TRUNC, 0644);
 	if (*fdout == -1)
 		perror("Error");
-	cmd2 = ft_split(argv[3], ' ');
+	cmd2 = ft_split(argv[argc -2], ' ');
 	path_cmd = parser_path(cmd2[0], env);
 	argv_cmd2[0] = cmd2[0];
 	argv_cmd2[1] = cmd2[1];
